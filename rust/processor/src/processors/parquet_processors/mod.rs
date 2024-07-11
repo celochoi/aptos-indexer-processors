2ad2327b4f51462fad4d794ad35f4d6b05646d6a
yuunlimm <dla0267@gmail.com>
2024-07-11 11:46:09 -0700

    fix build

--- rust/processor/src/processors/parquet_processors/mod.rs
+++ rust/processor/src/processors/parquet_processors/mod.rs
@@ -2,7 +2,6 @@ use std::time::Duration;
 
 pub mod parquet_ans_processor;
 pub mod parquet_default_processor;
-
 pub mod parquet_fungible_asset_processor;
 
 pub const GOOGLE_APPLICATION_CREDENTIALS: &str = "GOOGLE_APPLICATION_CREDENTIALS";
